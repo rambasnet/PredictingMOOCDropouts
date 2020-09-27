d70ac065f464a8548b469e7062f99aa0c709caf3
Ram Basnet <rambasnet@gmail.com>
2020-09-26 21:11:33 -0600

    added kdd scripts

--- data_download.sh
+++ data_download.sh
@@ -4,14 +4,17 @@
 
 echo "Downloading prediction data from server"
 file="data/prediction_data.tar.gz"
+
 if [[ -f $file ]]; then
     echo "$file exists."
 else
+    echo "Downloading $file..."
     curl -s -o $file http://lfs.aminer.cn/misc/moocdata/data/prediction_data.tar.gz
     echo "Extracting files from $file..."
     tar -C data -xzvf data/prediction_data.tar.gz
     echo "Done extracting files."
 fi
+
 file="data/user_info.csv"
 if [[ -f $file ]]; then
     echo "$file exists."
@@ -22,10 +25,21 @@ fi
 
 file="data/course_info.csv"
 if [[ -f $file ]]; then
-    echo "$file exists"
+    echo "$file exists."
 else
     echo "Downloading $file..."
     curl -s -o $file http://lfs.aminer.cn/misc/moocdata/data/course_info.csv
+fi
+
+file="data/kddcup15.zip"
+if [[ -f $file ]]; then
+    echo "$file exists."
+else
+    echo "Downloading $file"
+    curl -s -o $file http://lfs.aminer.cn/misc/moocdata/data/kddcup15.zip
+    echo "Extracting files from $file"
+    unzip -o $file -d data # unzip without asking to data folder
+    echo "Done extracting files."
 fi
 
 echo "All done..."
